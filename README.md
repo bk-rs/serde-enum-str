e670b98da97cbf1f7dad97625b1bdc0d05047e71
vkill <vkill.net@gmail.com>
2021-07-10 23:39:39 +0800

    Update

---
+++ README.md
@@ -0,0 +1,19 @@
+# serde-enum-str
+
+* [Cargo package](https://crates.io/crates/serde-enum-str)
+
+## Solves
+
+https://stackoverflow.com/questions/57469527
+
+## Dev
+
+```
+cargo clippy --all-features -- -D clippy::all
+cargo +nightly clippy --all-features -- -D clippy::all
+
+cargo fmt -- --check
+
+cargo build-all-features
+cargo test-all-features -- --nocapture
+```
--- tests/test.rs
+++ tests/test.rs
@@ -33,3 +33,59 @@ mod simple {
         );
     }
 }
+
+mod without_rename {
+    use super::*;
+
+    #[derive(Deserialize_enum_str, Serialize_enum_str, PartialEq, Debug)]
+    enum Foo {
+        A,
+        B,
+        #[serde(other)]
+        Other(String),
+    }
+
+    #[test]
+    fn test_ser() {
+        assert_eq!(serde_json::to_string(&Foo::A).unwrap(), r#""A""#);
+        assert_eq!(serde_json::to_string(&Foo::B).unwrap(), r#""B""#);
+        assert_eq!(
+            serde_json::to_string(&Foo::Other("c".to_owned())).unwrap(),
+            r#""c""#
+        );
+    }
+
+    #[test]
+    fn test_de() {
+        assert_eq!(serde_json::from_str::<Foo>(r#""A""#).unwrap(), Foo::A);
+        assert_eq!(serde_json::from_str::<Foo>(r#""B""#).unwrap(), Foo::B);
+        assert_eq!(
+            serde_json::from_str::<Foo>(r#""c""#).unwrap(),
+            Foo::Other("c".to_owned())
+        );
+    }
+}
+
+mod without_other {
+    use super::*;
+
+    #[derive(Deserialize_enum_str, Serialize_enum_str, PartialEq, Debug)]
+    #[serde(rename_all = "snake_case")]
+    enum Foo {
+        A,
+        #[serde(rename = "B")]
+        B,
+    }
+
+    #[test]
+    fn test_ser() {
+        assert_eq!(serde_json::to_string(&Foo::A).unwrap(), r#""a""#);
+        assert_eq!(serde_json::to_string(&Foo::B).unwrap(), r#""B""#);
+    }
+
+    #[test]
+    fn test_de() {
+        assert_eq!(serde_json::from_str::<Foo>(r#""a""#).unwrap(), Foo::A);
+        assert_eq!(serde_json::from_str::<Foo>(r#""B""#).unwrap(), Foo::B);
+    }
+}
